c8385de84af809278983dbb16f063cdd88b42fda
zentsov.mikhail <zentsov.mikhail@yandex.ru>
2023-06-26 19:25:35 +0300

    style: fix style

--- src/main/java/ru/practicum/shareit/util/Constants.java
+++ src/main/java/ru/practicum/shareit/util/Constants.java
@@ -1,5 +1,5 @@
 package ru.practicum.shareit.util;
 
 public class Constants {
-    public static final String REQUEST_HEADER_USER_ID= "X-Sharer-User-Id";
+    public static final String REQUEST_HEADER_USER_ID = "X-Sharer-User-Id";
 }
